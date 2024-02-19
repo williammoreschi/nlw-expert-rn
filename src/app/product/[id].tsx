3d2afbdc3a3ebdf0fb2b756755e9cc6b020734f1
william <william.moreschi@guiadomarceneiro.net>
2024-02-19 08:54:10 -0300

    Corrigindo tela de produto: adicionando título ausente e implementando redirecionamento com Expo-Router para casos em que o produto não existe

--- src/app/product/[id].tsx
+++ src/app/product/[id].tsx
@@ -1,5 +1,5 @@
 import { Image, Text, View } from "react-native"
-import { useLocalSearchParams, useNavigation } from "expo-router"
+import { useLocalSearchParams, useNavigation, Redirect } from "expo-router"
 import { Feather } from "@expo/vector-icons"
 import { Button } from "@/components/button"
 import { LinkButton } from "@/components/link-button"
@@ -12,17 +12,24 @@ export default function Product(){
   const navigation = useNavigation()
   const { id } = useLocalSearchParams()
 
-  const product = PRODUCTS.filter((item) => item.id === id)[0]
+  const product = PRODUCTS.find((item) => item.id === id)
 
   function handleAddProduct(){
-    cartStore.add(product)
-    navigation.goBack()
+    if(product){
+      cartStore.add(product)
+      navigation.goBack()
+    }
+  }
+
+  if(!product){
+    return <Redirect href="/" />
   }
 
   return (
     <View className="flex-1">
       <Image source={product.cover} className="w-full h-52" resizeMode="cover" />
       <View className="p-5 mt-8 flex-1">
+      <Text className="text-xl font-heading text-white">{product.title}</Text>
       <Text className="text-2xl font-heading my-2 text-lime-400">{formatCurrency(product.price)}</Text>
       <Text className="font-body text-base leading-6 mb-6 text-slate-400">{product.description}</Text>
       {
